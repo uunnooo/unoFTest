90e4f5889f129a5da3217911ea40a0027ce53869
uunnooo <uno0522@gmail.com>
2021-11-18 17:01:07 +0900

    11/18

--- MakeFinanceData.py
+++ MakeFinanceData.py
@@ -3,13 +3,32 @@
 import FinanceDataReader as fdr
 import yfinance as yf
 import pandas as pd
+import matplotlib.pyplot as plt
+import economicSignal
+
+import matplotlib
+
 from datetime import datetime
 
-pathF = 'F:\\정윤호\\FinanceData\\TestData\\'
+# pathF = 'D:\\정윤호\\FinanceData\\TestData\\'
+pathF = 'D:\\uno\\unoFinance\\'
+eList = economicSignal.reportList
+eDataList = pd.DataFrame()
 
-# ticker 선언
+for ticker in eList :
+    print(ticker)
+    eData = fdr.DataReader(ticker, data_source='fred')
+    eDataList = pd.concat([eDataList,eData],axis=1)
+
+
+stocks = fdr.StockListing('NASDAQ') # 나스닥
+
 ticker1 = 'AAPL'
 fTicker = yf.Ticker(ticker1)
+
+'''
+# ticker 선언
+
 
 sData = fdr.DataReader(ticker1, '2010-01-01', data_source='investing')
 fTmpData = fTicker.get_financials(freq='quarterly')
@@ -20,7 +39,7 @@ rData.reset_index(inplace = True)
 
 fTmpData = fTicker.get_financials(freq='quarterly')
 fTmpData.to_csv(pathF+'FinanceData.csv')
-
+'''
 
 
 
